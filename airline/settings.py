bd2a294ab3c6c97e5430fc6b791040fd2429c9fe
shirleynelson <star2jem@gmail.com>
2019-08-15 19:25:12 -0400

    OpenShift default Database

--- airline/settings.py
+++ airline/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'django',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1:5432',
         'PORT': 5432,
     }
 }
