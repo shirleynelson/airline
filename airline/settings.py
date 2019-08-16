b1a4e0d5b71ea2f62ea1cf1e281b94127c5b6458
shirleynelson <star2jem@gmail.com>
2019-08-15 20:18:20 -0400

    Try POSTGRES_USER POSTGRES_PASSWORD POSTGRES_HOST POSTGRES_PORT

--- airline/settings.py
+++ airline/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environment['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': 'default',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT',5432),
     }
 }
         #'NAME': os.environment['POSTGRESQL_DATABASE'],
