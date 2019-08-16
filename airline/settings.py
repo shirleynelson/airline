e27e41b55dde032966c1b403a4feea086f621d60
shirleynelson <star2jem@gmail.com>
2019-08-15 22:22:22 -0400

    update settings

--- airline/settings.py
+++ airline/settings.py
@@ -74,19 +74,31 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+import urlparse
+db_url = urlparse.urlparse(os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL'))
+
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['OPENSHIFT_APP_NAME'],
+    'USER': db_url.username,
+    'PASSWORD': db_url.password,
+    'HOST': db_url.hostname,
+    'PORT': db_url.port,
+    }
+}
 #DATABASES = {
 #    'default': database.config()
 #}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'default',
-        'USER': 'django',
-        'PASSWORD': 'password',
-        'HOST': '',
-        'PORT': '5432 '
-    }
-}
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': 'default',
+#       'USER': 'django',
+#       'PASSWORD': 'password',
+#       'HOST': '',
+#       'PORT': '5432 '
+#   }
+#
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
